30d4756380d46c6d7b47b0076d0cd3c811327122
Gajesh1722 <gsolanki1623@2A140-A01.conestogac.on.ca>
2015-11-13 14:15:21 -0500

    Mission Control Mobile app
    This is windows form based C# application. It has amazing graphics. I
    used google map here.

---
+++ MissionControlLab/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//using these following things
+using GMap.NET.WindowsForms.Markers;
+using GMap.NET.MapProviders;
+using GMap.NET.WindowsForms;
+
+namespace MissionControlLab
+{
+    public partial class Form1 : Form
+    {
+
+        GMapOverlay m_markersOverlay= new GMapOverlay();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void gMapControl1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Full Screen MAp
+            gMapControl1.SetBounds(0, 0, ClientRectangle.Width, ClientRectangle.Height);
+            //Full Screen Foreground
+            pictureBox1.SetBounds(0, 0, ClientRectangle.Width, ClientRectangle.Height);
+
+            gMapControl1.MapProvider = GMap.NET.MapProviders.ArcGIS_Imagery_World_2D_MapProvider.Instance;
+            GMap.NET.GMaps.Instance.Mode = GMap.NET.AccessMode.ServerOnly;
+
+
+            //drag and zoom facility
+            gMapControl1.MinZoom = 1;
+            gMapControl1.MaxZoom = 11;
+
+            pictureBox1.Parent = gMapControl1;
+
+            //for gray scale mode
+            gMapControl1.GrayScaleMode =true;
+
+            gMapControl1.Position = new GMap.NET.PointLatLng(43.389758, -80.405068);
+            gMapControl1.Zoom = 11;
+
+            //Drop a waypoint to my location
+
+            /*GMarkerGoogle marker=new GMarkerGoogle(new GMap.NET.PointLatLng
+                                                        (43.389758,-80.405068),
+                                                        GMarkerGoogleType.green_dot);
+            m_markersOverlay.Markers.Add(marker);
+            gMapControl1.Overlays.Add(m_markersOverlay);*/
+
+
+            pictureBox2.Parent = gMapControl1;
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+            //Drop a waypoint to my location give or take
+
+            var r= new Random();
+
+            double randomLat=43.389758+0.0001*r.Next(-100,100);
+            double randomLon=-80.405068+0.0001*r.Next(-100,100);
+
+
+
+            GMarkerGoogle marker = new GMarkerGoogle(new GMap.NET.PointLatLng
+                                                        (randomLat, randomLon),
+                                                        GMarkerGoogleType.green_dot);
+            m_markersOverlay.Markers.Add(marker);
+            gMapControl1.Overlays.Add(m_markersOverlay);
+        }
+    }
+}
